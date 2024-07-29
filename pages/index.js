c0a8a3547dee5c0e5fa0df74d8c8d04a5c1cbcd1
A_Ariannejad <ariannejad14@gmail.com>
2024-07-29 18:46:47 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import Avatar from "../components/Avatar";
 import { motion } from "framer-motion";
 //variants
 import { fadeIn } from "../variants";
-import About from "./about/index"
+import About from "./about/index";
+import SimpleBar from "simplebar-react";
+
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
@@ -23,17 +25,19 @@ const Home = () => {
             {"Hi, I'm Arshia Ariannejad"} <br /> Into{" "}
             <span className="text-accent">Computer Engineering </span>
           </motion.h1>
-          <motion.p
+          <motion.div
             variants={fadeIn("down", 0.3)}
             initial="hidden"
             animate="show"
             exit={"hidden"}
-            className="max-w-sm xl:max-w-4xl mx-auto xl:mx-0 mb-10 xl:mb-16 max-h-52 xl:max-h-fit overflow-auto scrollbar scrollbar-thumb-[#1b1738] scrollbar-thumb-rou   scrollbar-thin "
+            className="w-4/5 xl:max-w-4xl mx-auto xl:mx-0 mb-10 xl:mb-16  xl:max-h-fit overflow-auto scrollbar scrollbar-thumb-[#1b1738] scrollbar-thumb-rou   scrollbar-thin "
           >
-            {
-              "I am an experienced Backend Developer specializing in Websites and Video Games. I am passionate about seeking new opportunities to expand my knowledge and create innovative projects. As a dedicated and fast learner, I thrive in dynamic environments where I can continuously learn and grow.My ultimate goal is to develop products that resonate with users and provide them with an enjoyable experience. I am driven by the desire to create something that people will love and use on a daily basis. I am open to exciting challenges and collaborations that allow me to push the boundaries of what's possible in the world of technology. If you are looking for a talented and motivated developer to join your team, I would love to connect and discuss how my skills and passion can contribute to your projects. Let's create something amazing together!"
-            }
-          </motion.p>
+            <SimpleBar style={{ height: 400 }}>
+              {
+                "I am an experienced Backend Developer specializing in Websites and Video Games. I am passionate about seeking new opportunities to expand my knowledge and create innovative projects. As a dedicated and fast learner, I thrive in dynamic environments where I can continuously learn and grow.My ultimate goal is to develop products that resonate with users and provide them with an enjoyable experience. I am driven by the desire to create something that people will love and use on a daily basis. I am open to exciting challenges and collaborations that allow me to push the boundaries of what's possible in the world of technology. If you are looking for a talented and motivated developer to join your team, I would love to connect and discuss how my skills and passion can contribute to your projects. Let's create something amazing together!"
+              }
+            </SimpleBar>
+          </motion.div>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative ">
             <ProjectsBtn></ProjectsBtn>
