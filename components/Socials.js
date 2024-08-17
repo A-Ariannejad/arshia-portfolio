ab49b833781d8a07ab98d275c4d2c22504e52f1b
A_Ariannejad <ariannejad14@gmail.com>
2024-08-17 18:13:07 +0330

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -35,7 +35,7 @@ const Socials = () => {
       <Link
         className="text-accent"
         href={
-          "https://drive.google.com/file/d/1rj093iVUBSN5vJg5oxAo6sI8OtMDP1f7/view?usp=sharing"
+          "https://drive.google.com/file/d/1GW2D8YYNQaPakWAu6mWWwMAGo5yR16xR/view?usp=sharing"
         }
         download
       >
